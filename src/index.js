965df8bcf4a28ecbf0387db4accc1d9c29405ea9
Daniel Oshiro <webgoal@iMac-de-Igor-Marketing.local>
2019-02-11 15:22:47 -0200

    Removendo dependências do bootstrap e adicionando apenas o css.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import App from './main/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
